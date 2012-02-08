aa6f1f073b4bf1f60a24059c458bd087a678bd58
aylr <taylormiller@gmail.com>
2012-02-08 07:26:33 -0700

    Added jQuery links and basic row loop structure
    Note row_chunk.htm.js is a starting point for creating the dynamically
    generated row loop. There are a lot of quotes(") that need to be
    escaped in javascript strings with \". Before I go and do all that, I
    want to 1) decide on what the form elements need in terms of unique
    "id", "name", etc and 2) make sure it is more efficient to build the
    form via a loop rather than have it static.

---
+++ SD Web Files/scripts.js
@@ -0,0 +1,22 @@
+// Scripts for theHB
+// consider building the gui.htm form via a loop that changes row numbers as
+// the index increases. This may save slightly on bandwith from the server
+
+$("document").ready(function(){
+	
+	build_form(10);
+	
+	function build_form(num){
+		if(num == undefined){			//set default if num isn't passed w/ function
+			num = 6;
+		}
+		if(num < 11){					//minor sanity check with number of rows
+			for(i=1;i<=num;i++){		//loop through to build rows
+				//build actual row here
+				$("form").append("<div class=\"last\" id=\"row" + i + "\">row" + i + "</div>");
+			}
+		}else{
+			alert("Form is too big. Keep less than 10 rows.");
+		}
+	}//end build_form()
+});//end doc.ready
