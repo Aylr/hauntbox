97f0cc19a6f9020a1deca5aeb01e1311dac08188
aylr <taylormiller@gmail.com>
2012-04-12 22:09:19 -0600

    Mega cleanup to serve from SD card.
    Arduino can only handle 8.3 names and can't do folders, so this massive
    looking commit really isn't all that. Just renamed a bunch of files,
    and moved a bunch more. Tiny changes include changing urls in .css
    files and in the html .js and .css refs.

--- SD Web Files/hbscript.js
+++ SD Web Files/hbscript.js
@@ -1,6 +1,4 @@
 // Scripts for theHB
-// consider building the gui.htm form via a loop that changes row numbers as
-// the index increases. This may save slightly on bandwith from the server
 
 $("document").ready(function(){
 
