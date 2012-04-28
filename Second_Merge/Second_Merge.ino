b46c7a9f41f176b1febde0280898ad81a2baad53
aylr <taylormiller@gmail.com>
2012-04-28 14:28:56 -0600

    Progress on Second Merge
    Moved functional block from gerneric_server.ino to Second_merge.ino to
    begin the merge process. Mostly trying to figure out how to finish the
    bridge by working with guiFlag.

--- Second_Merge/Second_Merge.ino
+++ Second_Merge/Second_Merge.ino
@@ -13,10 +13,10 @@
 int d = 0;  //Delay used in testing of code.  Set to 0 if not testing code.
 
 //--------------------Define/get variables from GUI-------------------------------------
-int guiFlag = 1;  //GUI Flag tells us when there are new definitions from the GUI
+int guiFlag = 0;  //GUI Flag tells us when there are new definitions from the GUI
 
 //THESE VALUES ARE SET ARBITRARILY TO TEST CODE.  WILL NEED TO CHANGE IN FINAL VERSION!
-int inputArray[] =       {6, 6, 6, 6, 6, 6};    //which input (0-6) is read by which row ({row0, row1, ...})
+int inputArray[] =       {1, 6, 6, 6, 6, 6};    //which input (0-6) is read by which row ({row0, row1, ...})
                                                 //0 = no input, 1 = input #1, 2 = input #2, etc
 
 int inputHiLowArray[] =  {1, 1, 1, 1, 1, 0};    //What signal level is considered "on" for input # ({input1, input2, ...})
@@ -26,8 +26,8 @@ int outputArray[] =      {1, 2, 3, 4, 5, 6};    //which outputs (0-6) are contro
                                                 //0 = no output, 1 = output #1, 2 = output #2, etc
                                                 
 int outputHiLowArray[] = {1, 1, 1, 1, 1, 1};    //Output considered on when High (1) or Low (0)
-int DelayRow[] = {1000, 2000, 3000, 4000, 5000, 6000};     //Time in millis  
-int DurationRow[] = {6000, 6000, 6000, 6000, 6000, 6000};  //Time in millis  //TURN INTO AN ARRAY FOR FINAL CODE
+int DelayRow[] = {0, 2000, 3000, 4000, 5000, 6000};     //Time in millis  
+int DurationRow[] = {1000, 6000, 6000, 6000, 6000, 6000};  //Time in millis  //TURN INTO AN ARRAY FOR FINAL CODE
 
 
 //----------------------Define variables in code-----------------------------
@@ -67,10 +67,32 @@ int pinOut6 = 2; //Digital pin
 // Pin 12 (MISO)
 // Pin 13 (SCK)
 
-boolean file_handler(TinyWebServer& web_server);          // FIND OUT WHAT THIS IS/MEANS!!!
-//boolean blink_led_handler(TinyWebServer& web_server);
-//boolean led_status_handler(TinyWebServer& web_server);
+
+/****************VALUES YOU CHANGE*************/
+// pin 4 is the SPI select pin for the SDcard
+const int SD_CS = 4;
+
+// pin 10 is the SPI select pin for the Ethernet
+const int ETHER_CS = 10;
+
+// Don't forget to modify the IP to an available one on your home network
+byte ip[] = { 192, 168, 1, 9 };
+//byte ip[] = {10,0,0,10 };
+
+char StorageString[100];
+
+/*********************************************/
+static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char* browser_header = "HTTP/1.0 200 OK\nContent-Type: text/html\n";  //2 line header including mandatory blank line to signify data below
+
+//--------------------------- Define Handlers ----------------------------
+
+boolean file_handler(TinyWebServer& web_server);
 boolean index_handler(TinyWebServer& web_server);
+boolean test_handler(TinyWebServer& web_server);
+boolean program_handler(TinyWebServer& web_server);
+boolean settings_handler(TinyWebServer& web_server);
+boolean export_handler(TinyWebServer& web_server);
 
 TinyWebServer::PathHandler handlers[] = {
   // Work around Arduino's IDE preprocessor bug in handling /* inside
@@ -79,9 +101,12 @@ TinyWebServer::PathHandler handlers[] = {
   // `put_handler' is defined in TinyWebServer
  // {"/", TinyWebServer::GET, &index_handler },
   {"/", TinyWebServer::GET, &index_handler },
-  {"/gui", TinyWebServer::GET, &gui_handler },
-    {"/ram", TinyWebServer::GET, &ram_handler },
+  {"/ram", TinyWebServer::GET, &ram_handler },
   {"/status", TinyWebServer::GET, &status_handler },
+  {"/program", TinyWebServer::GET, &program_handler },
+  {"/testget", TinyWebServer::POST, &testget_handler },
+  {"/settings", TinyWebServer::GET, &settings_handler },
+  {"/export", TinyWebServer::GET, &export_handler },
   {"/upload/" "*", TinyWebServer::PUT, &TinyWebPutHandler::put_handler },
   {"/" "*", TinyWebServer::GET, &file_handler },
   {NULL},
@@ -99,13 +124,6 @@ Sd2Card card;
 SdVolume volume;
 SdFile root;
 SdFile file;
-
-
-// -------------------- Settings you should modify -------------------- 
-
-static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-// Don't forget to modify the IP to an available one on your home network
-byte ip[] = { 192, 168, 1, 9 };
 
 
 // -------------------- send file handler -------------------- 
@@ -143,17 +161,277 @@ boolean file_handler(TinyWebServer& web_server) {
 // -------------------- index handler -------------------- 
 
 boolean index_handler(TinyWebServer& web_server) {
-  send_file_name(web_server, "INDEX.HTM");
+  send_file_name(web_server, "gui.htm");
   return true;
 }
 
 
-// -------------------- gui handler -------------------- 
+boolean testget_handler(TinyWebServer& web_server){
+  
+  char* col[6];
+  char* tok;
+  byte input_arr[6];
+  byte in_onoff[6];
+  unsigned int ondelay[6];
+  byte out_arr[6];
+  byte out_onoff[6];
+  unsigned int duration[6];
+  
+  byte i = 0;
+  
+  // String readString = String(); //string for fetching data from address
 
-boolean gui_handler(TinyWebServer& web_server) {
-    send_file_name(web_server, "GUI.HTM");
-    return true;
+  // We're using a character array instead of the built-in String class because
+  // that's what the strtok function needs.  Plus, using Strings in this case
+  // wouldn't provide us a lot of benefit anyway.
+  // You need to decide how many bytes your server could possibly send.  This
+  // is a nice round number as a placeholder.  Also note that this is the
+  // maximum size that the variable "byte i" will index.  If you need a bigger
+  // array, make i an int.  
+
+  char readString[100];
+  char ch;
+  Client& client = web_server.get_client();
+  Serial.print("Free RAM: ");
+  Serial.println(FreeRam());
+
+  while (client.connected()) {
+    //Serial.print("1: con = ");
+    //Serial.print(client.connected());
+    //Serial.print(", avail = ");
+    //Serial.println(client.available());
+    
+    if (client.available() > 0) {      //if there are incoming bytes
+      //Serial.print("2: con = ");
+      //Serial.print(client.connected());
+      //Serial.print(", avail = ");
+      //Serial.println(client.available());
+      
+      ch = client.read();
+      
+      //if (readString.length() < 100) {//need to find out what this limit does
+      // Read a byte at a time and concatenate it onto the end of the character array.
+      readString[i] = ch;
+      StorageString[i] = ch;
+      i++;
+      //}
+          
+    }else if (client.available() == 0) {
+      //Serial.print("3: con = ");
+      //Serial.print(client.connected());
+      //Serial.print(", avail = ");
+      //Serial.println(client.available());
+      
+      Serial.println(readString);
+      //StorageString = readString;
+      	
+      // Split readString into columns with comma-separated values.  Columns
+      // are separated by semicolons.  The first time you call strtok, you
+      // pass the string (char array) you want to work on.  It returns a
+      // a string with everything up to the delimiter you set.  For
+      // subsequent calls, you pass NULL, and the function keeps working on
+      // the original string.  Since we've set up the client to send 6
+      // columns, we call the function 6 times.  The check for tok == NULL is
+      // there in case there's a problem and we don't get the full packet.
+      // This may help it fail more gracefully.
+      tok = strtok(readString, ";");
+      col[0] = tok;
+      for (i = 1; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        tok = strtok(NULL, ";");
+        col[i] = tok;
+      }
+      
+      // Now turn the array of strings into arrays of numbers.  Each array is
+      // named for the column it represents.  The values are separated by
+      // commas.  atoi is used to convert the stringified numbers back into
+      // integers.  The values returned by atoi, which are ints, are cast
+      // into the appropriate data type.  (That's what the (byte) before
+      // atoi(tok) is doing.)  It would be more graceful to create a function
+      // to do this, rather than repeat it 6 times.
+      // input_arr
+      tok = strtok(col[0], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        input_arr[i] = (byte)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      // in_onoff
+      tok = strtok(col[1], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        in_onoff[i] = (byte)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      // ondelay
+      tok = strtok(col[2], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        ondelay[i] = (unsigned int)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      // out_arr
+      tok = strtok(col[3], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        out_arr[i] = (byte)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      // out_onoff
+      tok = strtok(col[4], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        out_onoff[i] = (byte)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      // duration
+      tok = strtok(col[5], ",");
+      for (i = 0; i < 6; i++) {
+        if (tok == NULL)
+          break;
+        duration[i] = (unsigned int)atoi(tok);
+        tok = strtok(NULL, ",");
+      }
+      
+      // Now print out all the values to make sure it all worked
+      Serial.print("Input array: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(input_arr[i]); Serial.print(" ");
+      
+      Serial.print("\nInput on/off: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(in_onoff[i]); Serial.print(" ");
+      
+      Serial.print("\nOn delay: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(ondelay[i]); Serial.print(" ");
+      
+      Serial.print("\nOutput array: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(out_arr[i]); Serial.print(" ");
+      
+      Serial.print("\nOutput on/off: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(out_onoff[i]); Serial.print(" ");
+      
+      Serial.print("\nDuration: ");
+      for (i = 0; i < 6; i++)
+        Serial.print(duration[i]); Serial.print(" ");
+      
+      client.println(browser_header);
+      //send some sample data back to the broswer
+      client.print("Free RAM: ");
+      client.print(FreeRam());
+      client.print(", millis: ");
+      client.println(millis());
+      
+      //Data sending protocol (Arduino --> Browser)
+      //Input array;Input on/off;On delay;Output array;Output on/off;Duration;status code;Free RAM;
+      //0,0,0,0,0,0;1,1,1,1,1,1;0,0,0,0,0,0;0,0,0,0,0,0;1,1,1,1,1,1;0,0,0,0,0,0;0;182;
+      //status codes
+      //0 = all good
+      //1 = 
+      //2 = 
+      //3 = 
+      //4 = 
+      client.print("Input array: ");
+      for (i = 0; i < 6; i++)
+        client.print(input_arr[i]); client.print(" ");
+        
+      client.print("\nInput on/off: ");
+      for (i = 0; i < 6; i++)
+        client.print(in_onoff[i]); client.print(" ");
+      
+      client.print("\nOn delay: ");
+      for (i = 0; i < 6; i++)
+        client.print(ondelay[i]); client.print(" ");
+      
+      client.print("\nOutput array: ");
+      for (i = 0; i < 6; i++)
+        client.print(out_arr[i]); client.print(" ");
+      
+      client.print("\nOutput on/off: ");
+      for (i = 0; i < 6; i++)
+        client.print(out_onoff[i]); client.print(" ");
+      
+      client.print("\nDuration: ");
+      for (i = 0; i < 6; i++)
+        client.print(duration[i]); client.print(" ");
+      
+      client.print("\n");
+      
+      client.stop();
+
+      //Serial.print("4: con = ");
+      //Serial.print(client.connected());
+      //Serial.print(", avail = ");
+      //Serial.println(client.available());
+      
+    }//client.available()
+    
+    //Serial.print("5: con = ");
+    //Serial.print(client.connected());
+    //Serial.print(", avail = ");
+    //Serial.println(client.available());
+    
+  }//while client.connected()
+  
+    //Serial.print("6: con = ");
+    //Serial.print(client.connected());
+    //Serial.print(", avail = ");
+    //Serial.println(client.available());
+              
+  return true; //exit the handler 
 }
+
+
+boolean program_handler(TinyWebServer& web_server){
+  send_file_name(web_server, "program.txt");
+  
+  Client& client = web_server.get_client();
+  //Serial.print("Free RAM: ");
+  //Serial.println(FreeRam());
+  //client.println(browser_header);
+
+  //client.print("0,2,3,4,5,6;0,1,0,1,0,1;0,1,10,100,500,900;6,5,4,3,2,1;0,1,0,1,0,1;0,1,10,100,500,900;0;");
+  client.print(";0;");
+  client.print(FreeRam());
+  client.print(";");
+  client.print("\n");
+  client.stop();
+  return true; //exit the handler 
+}
+
+boolean settings_handler(TinyWebServer& web_server){
+  send_file_name(web_server, "settings.txt");
+
+  Client& client = web_server.get_client();
+  //client.println(browser_header);
+  
+  //client.print("0,1,0,0,1,0,0,0,1,0,0,0;input1,input2,input3,input4,input5,input6;output1,output2,output3,output4,output5,output6;103,246,492,103,246,492,103,246,492,103,246,492;1,2,3,4,5,6;0;");
+  client.print(";0;");    //send status code = 0
+  client.print(FreeRam());
+  client.print(";");
+  client.print("\n");
+  client.stop();
+  return true; //exit the handler 
+}
+
+boolean export_handler(TinyWebServer& web_server) {
+  Client& client = web_server.get_client();
+  client.println(browser_header);
+  
+  client.print("Date, Description\nSettings:0,1,0,0,1,0,0,0,1,0,0,0;input1,input2,input3,input4,input5,input6;output1,output2,output3,output4,output5,output6;103,246,492,103,246,492,103,246,492,103,246,492;1,2,3,4,5,6;\nProgram: 0,2,3,4,5,6;0,1,0,1,0,1;0,1,10,100,500,900;6,5,4,3,2,1;0,1,0,1,0,1;0,1,10,100,500,900;\n");
+  client.stop();
+  return true;
+}
+
 
 
 // -------------------- ram handler -------------------- 
@@ -198,6 +476,8 @@ void file_uploader_handler(TinyWebServer& web_server,
 			   char* buffer, int size) {
   static uint32_t start_time;
   static uint32_t total_size;
+  char* fname;
+  byte tempGuiFlag = 0;
 
   switch (action) {
   case TinyWebPutHandler::START:
@@ -206,10 +486,17 @@ void file_uploader_handler(TinyWebServer& web_server,
     if (!file.isOpen()) {
       // File is not opened, create it. First obtain the desired name
       // from the request path.
-      char* fname = web_server.get_file_from_path(web_server.get_path());
+      fname = web_server.get_file_from_path(web_server.get_path());
       if (fname) {
 	Serial << F("Creating ") << fname << "\n";
 	file.open(&root, fname, O_CREAT | O_WRITE | O_TRUNC);
+
+        //IF PROGRAM.TXT WAS UPDATED, TRIGGER UPDATE TO PROGRAM ARRAYS
+        Serial.println(fname);
+        if (fname == "PROGRAM.TXT"){
+          tempGuiFlag = 1;
+        }
+
 	free(fname);
       }
     }
@@ -228,6 +515,10 @@ void file_uploader_handler(TinyWebServer& web_server,
 	   << millis() - start_time << F(" millis (received ")
            << total_size << F(" bytes)\n");
     file.close();
+    if (tempGuiFlag == 1){
+      guiFlag = 1;
+      tempGuiFlag = 0;
+    }
   }
 }
 
@@ -237,17 +528,35 @@ void file_uploader_handler(TinyWebServer& web_server,
 // -------------------- begin firmware -------------------- 
 
 void setup() {
-   Serial.begin(9600);
-   Serial << F("Free RAM: ") << FreeRam() << "\n";
-    // -------------------- initialize SD card --------------------
+  Serial.begin(115200);
+  Serial << F("Free RAM: ") << FreeRam() << "\n";
+
+//  pinMode(LEDPIN, OUTPUT);
+//  setLedEnabled(false);
+
+  pinMode(SS_PIN, OUTPUT);	// set the SS pin as an output
+                                // (necessary to keep the board as
+                                // master and not SPI slave)
+  digitalWrite(SS_PIN, HIGH);	// and ensure SS is high
+
+  // Ensure we are in a consistent state after power-up or a reset
+  // button These pins are standard for the Arduino w5100 Rev 3
+  // ethernet board They may need to be re-jigged for different boards
+  pinMode(ETHER_CS, OUTPUT); 	// Set the CS pin as an output
+  digitalWrite(ETHER_CS, HIGH); // Turn off the W5100 chip! (wait for
+                                // configuration)
+  pinMode(SD_CS, OUTPUT);       // Set the SDcard CS pin as an output
+  digitalWrite(SD_CS, HIGH); 	// Turn off the SD card! (wait for
+                                // configuration)
+
+  // initialize the SD card.
   Serial << F("Setting up SD card...\n");
-  pinMode(10, OUTPUT); // set the SS pin as an output (necessary!)
-  digitalWrite(10, HIGH); // but turn off the W5100 chip!
-  if (!card.init(SPI_FULL_SPEED, 4)) {
+  // Pass over the speed and Chip select for the SD card
+  if (!card.init(SPI_FULL_SPEED, SD_CS)) {
     Serial << F("card failed\n");
     has_filesystem = false;
   }
-  // initialize a FAT volume
+  // initialize a FAT volume.
   if (!volume.init(&card)) {
     Serial << F("vol.init failed!\n");
     has_filesystem = false;
@@ -259,8 +568,10 @@ void setup() {
 
   if (has_filesystem) {
     // Assign our function to `upload_handler_fn'.
-    TinyWebPutHandler::put_handler_fn = file_uploader_handler;
+   TinyWebPutHandler::put_handler_fn = file_uploader_handler;
   }
+
+  // Initialize the Ethernet.
   Serial << F("Setting up the Ethernet card...\n");
   Ethernet.begin(mac, ip);
 
@@ -269,6 +580,8 @@ void setup() {
   web.begin();
 
   Serial << F("Ready to accept HTTP requests.\n");
+  
+  
   
    pinMode(pinOut1, OUTPUT);
    pinMode(pinOut2, OUTPUT);
@@ -449,10 +762,11 @@ void loop(){
   
 
   //----- Section AA9 ----- See if GUI definitions have changed
-  if(guiFlag = 0) {  //No new GUI definitions
-    return;  //break out of main funtion if no new definitions have come in.  POSSIBLY REMOVE THE HARD RETURN?
+  if(guiFlag == 0) {  //No new GUI definitions
+    //return;  //break out of main funtion if no new definitions have come in.  POSSIBLY REMOVE THE HARD RETURN?
   }
-  if(guiFlag = 1) {  //New GUI definitions
+  if(guiFlag == 1) {  //New GUI definitions
+  Serial.println("Program.txt updated");
   //----- Section AB1 -----
   // PUT FUNCTION HERE TO READ GUI DEFINITIONS
   //  int inputArray[] =       
@@ -499,6 +813,7 @@ void loop(){
   //Sends GUI confirmation message
   guiMess(5);  //GUI message #5
   
+  guiFlag = 0;
 }  
   
  
