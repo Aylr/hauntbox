b77f085be1a71b7844ca64ba5144608b3ccedf1f
darrencorey <darrencorey@gmail.com>
2013-08-17 14:20:56 -0600

    Much better program.
    Refigured to a more real user mode.  Added wave file sorting, random
    mode, and an info display when the user presses 'i' or 'h'.

--- sound_module/sound_module.ino
+++ sound_module/sound_module.ino
@@ -13,7 +13,10 @@
 #include "WaveUtil.h"
 #include "WaveHC.h"
 
-#define MAX_SOUNDS 25
+#define MAJOR_VER 0
+#define MINOR_VER 4
+#define MAX_SOUNDS 30
+#define FNAME_LGTH 12
 
 
 // ---------------------------------- Mode Variables ----------------------------------
@@ -23,11 +26,11 @@ bool random_mode = false;           // true if it should play random trigger sou
 bool SD_failed = true;              // true if SD card has failed, false if successful
 
 // ---------------------------------- Misc Variables ----------------------------------
-char* ambient_wav_filename = "AMBIENT.WAV";     // ambient wave file name
-char* trigger_wav_filename = "ALARM.WAV";
-char* random_mode_filename = "RANDOM.TXT";
-char* trigger_sounds[MAX_SOUNDS] = {"0","1","2","3","4","5","6","7","8","9","10"};
-byte trigger_sound_count = 11;                  // keeps track of how many trigger sounds you have on the SD
+char *ambient_wav_filename = "AMBIENT.WAV";     // ambient wave file name
+// char *trigger_wav_filename = "ALARM.WAV";
+char *random_mode_filename = "RANDOM.TXT";
+char trigger_sounds[MAX_SOUNDS][FNAME_LGTH];
+byte trigger_sound_count = 0;                   // keeps track of how many trigger sounds you have on the SD
 int OC_trigger_pin = 5;                         // OC trigger pin
 int logic_trigger_pin = 4;                      // Logic trigger pin
 int OC_trigger_threshold = 300;                 // OC trigger threshold that it must go below
@@ -46,20 +49,48 @@ WaveHC wave;      // This is the only wave (audio) object, since we will only pl
 
 // ---------------------------------- Utility Functions ----------------------------------
 
-int freeRam(void){          // return the number of bytes currently free in RAM, great for debugging!
-  extern int  __bss_end; 
-  extern int  *__brkval; 
-  int free_memory; 
-  if((int)__brkval == 0) {
-    free_memory = ((int)&free_memory) - ((int)&__bss_end); 
+// Print device info to the serial port
+void print_info(void)
+{
+  byte i;
+
+  Serial.print(F("\r\nHauntbox Sound Module\r\nVersion "));
+  Serial.print(MAJOR_VER);
+  Serial.print(F("."));
+  Serial.println(MINOR_VER);
+
+  Serial.print(F("\r\nAmbient mode = "));
+  Serial.println(ambient_mode);
+  Serial.print(F("Random mode = "));
+  Serial.println(random_mode);
+
+  // Also play the trigger sounds
+  Serial.print(trigger_sound_count);
+  Serial.println(F(" trigger sounds found:"));
+  for (i = 0; i < trigger_sound_count; i++) {
+    Serial.println(trigger_sounds[i]);
+    playcomplete(trigger_sounds[i]);
   }
-  else {
-    free_memory = ((int)&free_memory) - ((int)__brkval); 
-  }
-  return free_memory; 
+  Serial.print(F("Current trigger index = "));
+  Serial.println(current_trigger_sound);
 }
 
-void sdErrorCheck(void){
+
+
+int freeRam(void) {         // return the number of bytes currently free in RAM, great for debugging!
+  extern int  __bss_end;
+  extern int  *__brkval;
+  int free_memory;
+  if ((int)__brkval == 0) {
+    free_memory = ((int)&free_memory) - ((int)&__bss_end);
+  }
+  else {
+    free_memory = ((int)&free_memory) - ((int)__brkval);
+  }
+  return free_memory;
+}
+
+void sdErrorCheck(void) {
   if (!card.errorCode()) return;
   Serial.print(F("\r\nSD I/O error: 0x"));
   Serial.print(card.errorCode(), HEX);
@@ -68,67 +99,67 @@ void sdErrorCheck(void){
   SD_failed = true;
 }
 
-void IO_pin_setup(){
-    // Set the output pins for the DAC control.
-    // These pins are defined in the library
-    pinMode(2, OUTPUT);
-    pinMode(3, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(5, OUTPUT);
+void IO_pin_setup() {
+  // Set the output pins for the DAC control.
+  // These pins are defined in the library
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 
-    //Set the LED output pins
-    pinMode(trigger_LED_pin, OUTPUT);
-    pinMode(reset_LED_pin, OUTPUT);
-    // pinMode(OC_trigger_pin, INPUT);
-    // pinMode(logic_trigger_pin, INPUT);
+  //Set the LED output pins
+  pinMode(trigger_LED_pin, OUTPUT);
+  pinMode(reset_LED_pin, OUTPUT);
+  // pinMode(OC_trigger_pin, INPUT);
+  // pinMode(logic_trigger_pin, INPUT);
 }
 
-bool setup_SD_card(){
-    //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
-    if (!card.init()) {         //play with 8 MHz spi (default faster!)  
-        Serial.println(F("SD init failed!"));  	// Something went wrong, lets print out why
-        sdErrorCheck();
-        SD_failed = true;							// set global mode variable
-        return false;
-    }
+bool setup_SD_card() {
+  //  if (!card.init(true)) { //play with 4 MHz spi if 8MHz isn't working for you
+  if (!card.init()) {         //play with 8 MHz spi (default faster!)
+    Serial.println(F("SD init failed!"));   // Something went wrong, lets print out why
+    sdErrorCheck();
+    SD_failed = true;             // set global mode variable
+    return false;
+  }
 
-    card.partialBlockRead(true);					// enable optimize read - some cards may timeout. Disable if you're having problems
+  card.partialBlockRead(true);          // enable optimize read - some cards may timeout. Disable if you're having problems
 
-    // Now we will look for a FAT partition!
-    uint8_t part;
-    for (part = 0; part < 5; part++) {     	// we have up to 5 slots to look in
-        if (vol.init(card, part)) 
-        break;                             	// we found one, lets bail
-    }
-    if (part == 5) {                       	// if we ended up not finding one  :(
-        Serial.println(F("No FAT partition!"));
-        sdErrorCheck(); 					// Something went wrong, lets print out why
-        SD_failed = true;					// set global mode variable
-    }
+  // Now we will look for a FAT partition!
+  uint8_t part;
+  for (part = 0; part < 5; part++) {      // we have up to 5 slots to look in
+    if (vol.init(card, part))
+      break;                              // we found one, lets bail
+  }
+  if (part == 5) {                        // if we ended up not finding one  :(
+    Serial.println(F("No FAT partition!"));
+    sdErrorCheck();           // Something went wrong, lets print out why
+    SD_failed = true;         // set global mode variable
+  }
 
-    // Lets tell the user about what we found
-    Serial.print(F("Using partition "));
-    Serial.print(part, DEC);
-    Serial.print(F(", type is FAT"));
-    Serial.println(vol.fatType(),DEC);     			// FAT16 or FAT32?
+  // Lets tell the user about what we found
+  Serial.print(F("Using partition "));
+  Serial.print(part, DEC);
+  Serial.print(F(", type is FAT"));
+  Serial.println(vol.fatType(),DEC);          // FAT16 or FAT32?
 
-    // Try to open the root directory
-    if (!root.openRoot(vol)) {
-        Serial.println(F("Can't open root dir!")); 	// Something went wrong
-        SD_failed = true;							// set global mode variable
-        return false;
-    }
+  // Try to open the root directory
+  if (!root.openRoot(vol)) {
+    Serial.println(F("Can't open root dir!"));  // Something went wrong
+    SD_failed = true;             // set global mode variable
+    return false;
+  }
 
-    SD_failed = false;                        // Important to set the SD_faled to success!
+  SD_failed = false;                        // Important to set the SD_faled to success!
 
-    Serial.println(F("SD Ready"));
+  Serial.println(F("SD Ready"));
 }
 
-void SD_failure_alert(){
-	// flash leds or something
+void SD_failure_alert() {
+  // flash leds or something
   digitalWrite(reset_LED_pin,HIGH);
-    Serial.println(F("SD failed."));
-    digitalWrite(reset_LED_pin,LOW);
+  Serial.println(F("SD failed."));
+  digitalWrite(reset_LED_pin,LOW);
 }
 
 
@@ -138,11 +169,11 @@ void SD_failure_alert(){
 //   for (uint8_t i = 0; i < 11; i++) {     // 8.3 format has 8+3 = 11 letters in it
 //     if (dir.name[i] == ' ')
 //         continue;         // dont print any spaces in the name
-//     if (i == 8) 
+//     if (i == 8)
 //         Serial.print('.');           // after the 8th letter, place a dot
 //     Serial.print(dir.name[i]);      // print the n'th digit
 //   }
-//   if (DIR_IS_SUBDIR(dir)) 
+//   if (DIR_IS_SUBDIR(dir))
 //     Serial.print('/');       // directories get a / at the end
 // }
 
@@ -187,16 +218,17 @@ int compare(void const *a, void const *b)
 void get_files(FatReader &d)
 {
   unsigned char c, skip;
-  char name[12];
+  char name[FNAME_LGTH];
   char *s = "";
   char *pch;
-  byte tsc;
-  char ts[MAX_SOUNDS][12];
-  
-  tsc = 0;
+  // char ts[MAX_SOUNDS][12];
+  // DARREN: Boundary check, don't get more than MAX_SOUNDS sounds
+  // DARREN: Check for good .wav file before adding to array
+
+  trigger_sound_count = 0;
   while ((d.readDir(dirBuf)) > 0) {     // read the next file in the directory
     // skip subdirs . and .. as well as hidden _filenames
-    if (dirBuf.name[0] == '.' || dirBuf.name[0] == '_') 
+    if (dirBuf.name[0] == '.' || dirBuf.name[0] == '_')
       continue;
 
     // Get the name of the next file
@@ -209,8 +241,8 @@ void get_files(FatReader &d)
     if (pch != NULL) {
       // Don't include ambient.wav.  It's special.
       if (strcmp(name, ambient_wav_filename) != 0) {
-        strcpy(ts[tsc], name);
-        tsc++;
+        strcpy(trigger_sounds[trigger_sound_count], name);
+        trigger_sound_count++;
       }
     }
   }
@@ -222,11 +254,11 @@ void get_files(FatReader &d)
   // }
 
   // Sort the array alphabetically
-  qsort(ts, tsc, 12, compare);
-  Serial.println(F("Sorted:"));
-  for (c = 0; c < tsc; c++) {
-    Serial.println(ts[c]);
-  }
+  qsort(trigger_sounds, trigger_sound_count, FNAME_LGTH, compare);
+  // Serial.println(F("Sorted:"));
+  // for (c = 0; c < trigger_sound_count; c++) {
+  //   Serial.println(trigger_sounds[c]);
+  // }
 
   sdErrorCheck();                  // are we doign OK?
 }
@@ -248,7 +280,7 @@ bool does_file_exist(char *name) {   // Should only really run at startup or if 
   return true;            // true if you made it this far without failing
 }
 
-bool trigger_check(){
+bool trigger_check() {
   if (analogRead(OC_trigger_pin) <= OC_trigger_threshold || analogRead(logic_trigger_pin) >= log_trigger_threshold) {
     Serial.print(F("OC: "));
     Serial.print(analogRead(OC_trigger_pin));
@@ -266,122 +298,117 @@ void playcomplete(char *name) {
   // call our helper to find and play this name
   playfile(name);
   while (wave.isplaying) {
-  // do nothing while its playing
+    // do nothing while its playing
   }
   // now its done playing
 }
 
-void playfile(char *name) {		// Plays w/ possiblity to be stopped
-  if (wave.isplaying) { 		// if wave object is already playing something, stop it!
-    wave.stop(); 				// stop it
+void playfile(char *name) {   // Plays w/ possiblity to be stopped
+  if (wave.isplaying) {     // if wave object is already playing something, stop it!
+    wave.stop();        // stop it
   }
-  if (!f.open(root, name)) {	// look in the root directory and open the file
+  if (!f.open(root, name)) {  // look in the root directory and open the file
     Serial.print(F("Couldn't open "));
+    // DARREN: check for sd card here! Check lots, shine led if problem
     Serial.println(name);
     return;
   }
-  if (!wave.create(f)) {		// OK read the file and turn it into a wave object
+  if (!wave.create(f)) {    // OK read the file and turn it into a wave object
     Serial.println(F("Not a valid WAV")); return;
   }
-  wave.play();					// ok time to play! start playback
+  wave.play();          // ok time to play! start playback
 }
 
 // Plays the ambient background sound while waiting for a trigger
 void playbackground(char *name) {
-  playfile(name);					// call our helper to find and play this name
-  while (wave.isplaying) {			// While the ambient background track is playing watch for triggers
+  playfile(name);         // call our helper to find and play this name
+  while (wave.isplaying) {      // While the ambient background track is playing watch for triggers
     if (trigger_check()) {
-      digitalWrite(trigger_LED_pin,HIGH);   	// turn on trigger LED
-      playcomplete(trigger_wav_filename);			// Play complete trigger wav
-      digitalWrite(trigger_LED_pin,LOW);      // turn off trigger LED
+      play_next_trigger();
     }
   }
-  // now its done playing
+  // now it's done playing
 }
 
-void play_next_trigger(){
-  if (!SD_failed){                                                 // if files exist
-    if (random_mode){                                               // if in random mode
-      playcomplete(trigger_sounds[random(0,(trigger_sound_count - 1))]); // play a random sound
-    }else{
+void play_next_trigger() {
+  if (!SD_failed) {                                                // if files exist
+    digitalWrite(trigger_LED_pin,HIGH);     // turn on trigger LED
+    if (random_mode) {                                              // if in random mode
+      playcomplete(trigger_sounds[random(0, trigger_sound_count)]); // play a random sound
+    } else {
       playcomplete(trigger_sounds[current_trigger_sound]);          // play the trigger sound
-      if (current_trigger_sound + 1 >= trigger_sound_count){       // if we've reached the end
-        current_trigger_sound = 0;                                  // back to the beginning
-      }else{
-        current_trigger_sound ++;
+      if (current_trigger_sound < (trigger_sound_count - 1)) {      // if we've reached the end
+        current_trigger_sound++;                                  // back to the beginning
+      } else {
+        current_trigger_sound = 0;
       }
     }
+    digitalWrite(trigger_LED_pin,LOW);      // turn off trigger LED
   }
 }
 
-void helper(char* number){
-  char temp[10] = {};
-  strcat(temp,number);
-  strcat(temp,".wav");
-  playcomplete(temp);
-}
+// void helper(char *number) {
+//   char temp[10] = {};
+//   strcat(temp,number);
+//   strcat(temp,".wav");
+//   playcomplete(temp);
+// }
 
 // ---------------------------------- Main Functions ----------------------------------
-void setup(){
-    Serial.begin(9600);                                 // set up serial port
-    Serial.print("Hauntbox Sound Module. Free RAM: ");  // Running out of RAM is bad
-    Serial.println(freeRam());                          // if this is under 150 bytes it may spell trouble!
-    IO_pin_setup();             						//setup all IO pins required
-    setup_SD_card();									//setup SD card
+void setup() {
+  Serial.begin(9600);                                 // set up serial port
+  Serial.print(F("Hauntbox Sound Module. Free RAM: "));  // Running out of RAM is bad
+  Serial.println(freeRam());                          // if this is under 150 bytes it may spell trouble!
+  IO_pin_setup();                         //setup all IO pins required
+  setup_SD_card();                  //setup SD card
 
-    //SD card logic
-    if (SD_failed) {                                    // if the SD failed
-        SD_failure_alert();								// alert user
-    } else {                                              // if the SD succeeded
+  //SD card logic
+  if (SD_failed) {                                    // if the SD failed
+    SD_failure_alert();               // alert user
+  } else {                                              // if the SD succeeded
 
-      // Whew! We got past the tough parts.
-      Serial.print(F("Files found:"));
-      // dirLevel = 0;
-      // Get all of the usable files on the SD card.
-      get_files(root);
+    // Whew! We got past the tough parts.
+    // Get all of the usable files on the SD card.
+    get_files(root);
 
-        //sound file logic
-        ambient_mode = does_file_exist(ambient_wav_filename);
-        Serial.print(F("ambient = "));
-        Serial.println(ambient_mode);
+    // sound file logic -- make sure these are usable wav files
+    ambient_mode = does_file_exist(ambient_wav_filename);
+    Serial.print(F("ambient = "));
+    Serial.println(ambient_mode);
 
-        for (byte i = 0; i < trigger_sound_count; i++) {
-          Serial.println(trigger_sounds[i]);
-          helper(trigger_sounds[i]);
-        }
-        // if (trig*.wav exists)
-        //     trigger_sounds[] = count of trig1.wav ... trign.wav
-        // if (file random.txt exists)
-        //     random_mode = true;
-    }
+    Serial.println(F("'i' for info"));
+
+    // for (byte i = 0; i < trigger_sound_count; i++) {
+    //   Serial.println(trigger_sounds[i]);
+    //   playcomplete(trigger_sounds[i]);
+    // }
+  }
 }
-// DARREN: command to display and play trigger sounds? Do normal operation.
-void loop(){
-    if (!SD_failed){                                    // if the SD succeeded
-        if (ambient_mode) {
-            playbackground(ambient_wav_filename);                // play background ambient listening for triggers
-        }
-        // if (triggered) {
-        //     digitalWrite(LED, ON);
-        //     if (trigger_sounds exists){
-        //         if (random_mode){                                                   // if in random_mode play random sound
-        //             play trigger_sounds[random number <= trigger_sounds.length];
-        //         }else{                                                              //if not in random_mode, play next sound in sequence
-        //             play trigger_sounds[current_trigger_sound];
-        //             if (current_trigger_sound > trigger_sounds.length){
-        //                 current_trigger_sound = 0;                                  // back to the beginning
-        //             }else{
-        //                 current_trigger_sound ++;
-        //             }
-        //         }
-        //     }
-        // }
-        // digitalWrite(LED,OFF)
-    }else{                                              // if the SD failed
-    	// setup_SD_card();								// try to setup SD card again
-     //  ambient_mode = does_file_exist(ambient_wav_filename);
-    	// if (SD_failed)									// if it failed after another attempt
-      SD_failure_alert();							// alert user
+
+
+void loop() {
+  unsigned char c;
+
+  if (!SD_failed) {                                   // if the SD succeeded
+    // Check for user input
+    if (Serial.available() > 0) {
+      c = Serial.read();
+      if (c == 104 || c == 105) { // 'h' or 'i'
+        print_info();
+      }
     }
+
+    if (ambient_mode) {
+      playbackground(ambient_wav_filename);                // play background ambient listening for triggers
+    }
+    else {
+      // Simply check for triggers
+      if (trigger_check()) {
+        play_next_trigger();
+      }
+    }
+  } else {                                             // if the SD failed
+    SD_failure_alert();             // alert user
+  }
 }
 
